198bf77e9a15393f01ab0e979f7a217207bd6149
RominaVoscio <89428394+RominaVoscio@users.noreply.github.com>
2022-02-23 19:59:38 -0300

    Desafio 2 clase3

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+
+
+const NavBar =()=> {
+    return(
+        <header>
+            <h1>COAPRO</h1>
+            <nav>
+                <a href="#">Liquidos</a>
+                <a href="#">Lentes</a>
+                <a href="#">Estuches </a>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar
